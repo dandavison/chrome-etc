44c9e45f6e091d849837d518f1081d7711159af9
Dan Davison <dandavison7@gmail.com>
2025-03-13 08:27:50 -0400

    Add host_permissions and clarify URL patterns

--- background.ts
+++ background.ts
@@ -5,7 +5,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 chrome.webNavigation.onCompleted.addListener(function (details) {
-  if (details.url.startsWith('http://wormhole') || details.url.startsWith('https://temporaltechnologies.slack.com/')) {
+  if (
+    details.url.startsWith('http://wormhole:7117/') ||
+    details.url.startsWith('https://temporaltechnologies.slack.com/') ||
+    details.url.startsWith('https://temporalio.slack.com/')
+  ) {
     chrome.tabs.remove(details.tabId);
   }
 });
