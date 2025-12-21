c320db0f3edb88e3be286a3f79ddacea02961137
Dan Davison <dandavison7@gmail.com>
2025-12-20 21:38:07 -0500

    fix: remove unused sendMessage that caused connection error
    No content script was listening for this message anyway.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,12 +1,3 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url && /^http/.test(tab.url)) {
-    // Send message but ignore errors if no listener exists
-    chrome.tabs.sendMessage(tabId, { command: 'initializeUI' }).catch(() => {
-      // Content script not listening - that's fine
-    });
-  }
-});
-
 chrome.webNavigation.onCompleted.addListener(function (details) {
   if (
     details.url.startsWith('http://wormhole:7117/') ||
