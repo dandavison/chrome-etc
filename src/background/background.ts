5b85554f0117b84d196d614d1789f61f1768b586
Dan Davison <dandavison7@gmail.com>
2026-01-30 15:21:47 -0500

    Don't handle wormhole URLs in this extension

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,5 @@
 chrome.webNavigation.onCompleted.addListener(function (details) {
   if (
-    details.url.startsWith('http://wormhole:7117/') ||
     details.url.startsWith('https://temporaltechnologies.slack.com/') ||
     details.url.startsWith('https://temporalio.slack.com/')
   ) {
